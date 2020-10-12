f2f87ed313e999a0c6464f27a2d47f69d6187c4c
Nagesh Singh Chauhan <nageshsinghc4@gmail.com>
2020-10-12 17:16:33 +0530

    Update setup.py

--- deepwrap/text/shallownlp/install/setup.py
+++ deepwrap/text/shallownlp/install/setup.py
@@ -1,48 +1,81 @@
+import sys
+
+if sys.version_info.major != 3:
+    raise Exception('DEEPWRAP requires Python 3.x')
+tf_version_str = 'tensorflow==2.3.0'
+if sys.version_info.minor == 8:
+    tf_version_str = 'tensorflow>=2.2.0'
+
 from distutils.core import setup
 import setuptools
 
-with open('README.md') as readme_file: 
+with open('README.md', encoding='utf-8') as readme_file:
     readme_file.readline()
     readme = readme_file.read()
-exec(open('shallownlp/version.py').read())
-
+exec(open('deepwrap/version.py').read())
 
 setup(
-  name = 'shallownlp',
-  packages = setuptools.find_packages(),
-  version = __version__,
-  license='MIT',
-  description = 'shallownlp is an easy-to-apply text analysis library for English and Chinese',
-  long_description = readme,
-  long_description_content_type = 'text/markdown',
-  author = 'Arun S. Maiya',
-  author_email = 'arun@maiya.net',
-  url = 'https://github.com/amaiya/ktrain',
-  keywords = ['nlp', 'text analytics', 'machine learning'],
-  install_requires=[
-          'scikit-learn == 0.21.3',
-          'langdetect',
-          'jieba',
-          'cchardet',
-          'syntok'
-      ],
-  classifiers=[  # Optional
-    # How mature is this project? Common values are
-    #   3 - Alpha
-    #   4 - Beta
-    #   5 - Production/Stable
-    'Development Status :: 4 - Beta',
+    name='DEEPWRAP',
+    packages=setuptools.find_packages(),
+    package_data={'DEEPWRAP': ['text/shallownlp/ner_models/*']},
+    version=setuptools.__version__,
+    license='Apache License 2.0',
+    description='DEEPWRAP is a wrapper for TensorFlow and Keras that makes deep learning and AI more accessible and '
+                'easier to apply',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='nageshsinghc4',
+    author_email="nageshsinghc4@gmail.com",
+    url = 'https://github.com/nageshsinghc4/deepwrap',
+    keywords=['tensorflow', 'keras', 'deep learning', 'machine learning'],
+    install_requires=[
+        tf_version_str,
+        'scipy==1.5.2',
+        # 'pillow'
+        'scikit-learn>=0.21.3',
+        'matplotlib >= 3.0.0',
+        'pandas >= 1.0.1',
+        'fastprogress >= 0.1.21',
+        'keras_bert>=0.81.0',
+        'requests',
+        'joblib',
+        'langdetect',
+        'theano',
+        'jieba',
+        'cchardet',
+        'networkx>=2.3',
+        'bokeh',
+        'seqeval',
+        'packaging',
+        'tensorflow_datasets',
+        'transformers>=2.11.0',  # due to breaking change in v2.11.0
+        'ipython',
+        'syntok',
+        'whoosh',
+        # these libraries are manually installed on-the-fly when required by an invoked method
+        # 'shap',  # used by TabularPredictor.explain
+        # 'eli5 >= 0.10.0', # forked version used by TextPredictor.explain and ImagePredictor.explain
+        # 'stellargraph>=0.8.2', # forked version used by graph module
+        # 'allennlp', # required for Elmo embeddings since TF2 TF_HUB does not work
+    ],
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
 
-    # Indicate who your project is intended for
-    'Intended Audience :: Developers',
-    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
 
-    # Pick your license as you wish
-    'License :: OSI Approved :: MIT License',
+        # Pick your license as you wish
+        'License :: OSI Approved :: Apache Software License',
 
-    # Specify the Python versions you support here. In particular, ensure
-    # that you indicate whether you support Python 2, Python 3 or both.
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-  ],
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
